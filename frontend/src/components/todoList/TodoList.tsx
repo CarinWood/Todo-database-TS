19cce4617cf86426aca2ff9d6bc569f5ae99fa87
CarinWood <carin.wood.85@gmail.com>
2022-04-11 23:05:29 +0200

    update frontend

--- frontend/src/components/todoList/TodoList.tsx
+++ frontend/src/components/todoList/TodoList.tsx
@@ -2,25 +2,37 @@ import React, {useState} from 'react';
 import './todoList.css'
 import InputArea from '../inputArea/InputArea';
 import TodoApiService from '../../api/services/TodoApiService';
+import Placeholder from '../placeholder/Placeholder';
 
 
 const TodoList = () => {
     const [todos, setTodos] = useState([])
 
+    interface Todo {
+        task: String
+        name: String
+        done: String
+    }
+
 
    const getTodos = () =>  {
         TodoApiService.getAllTodos()
         .then((response) => {
-            console.log(response.data)
+            setTodos(response.data)
         })
         .catch((error) => console.log(error))
     }
+    
     
 
     return (
         <div className='todo-list'>
             <InputArea/>
             <button onClick={() => getTodos()}>Get Todos</button>
+            
+            {todos.length > 0
+             ? todos.map((todo:Todo) => <p>{todo.task}</p>)
+             : <Placeholder/> }
     
         </div>
     )
