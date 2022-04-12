185f17d60a777d408eaba15136d586f218f03675
CarinWood <carin.wood.85@gmail.com>
2022-04-12 17:47:03 +0200

    update

--- frontend/src/components/inputArea/InputArea.tsx
+++ frontend/src/components/inputArea/InputArea.tsx
@@ -1,6 +1,7 @@
 import React, {FC, useState} from 'react'
 import './inputArea.css'
 import { BsPencilFill } from 'react-icons/bs';
+import { IoClose } from 'react-icons/io5';
 
 interface Props {
   addNewTodo: (value1: string, value2: string) => void;
@@ -11,25 +12,37 @@ const InputArea:FC<Props> = ({addNewTodo}) => {
     const [taskValue, setTaskValue] = useState('')
     const [nameValue, setNameValue] = useState('')
 
+    const handleClick = () => {
+      addNewTodo(taskValue, nameValue);
+      setTaskValue('');
+      setNameValue('');
+    }
+
+   
+
   return (
     <div className="input-container">
         <h1 className='todo-heading'>Todo List <BsPencilFill/></h1>
             <div className="input-fields">
-                <input 
-                    className="input-task" 
-                    placeholder='Task' 
-                    value={taskValue} 
-                    onChange={(event) => setTaskValue(event.target.value)}
-                />
+                 
+                        <input 
+                            className="input-task" 
+                            placeholder='Task' 
+                            value={taskValue} 
+                            onChange={(event) => setTaskValue(event.target.value)}
+                        />
+                       
+                 
                 <input 
                     className="input-name" 
                     placeholder='Your name'
                     value={nameValue}
                     onChange={(event) => setNameValue(event.target.value)}
                 />
+                    
 
 
-                <button className="add-btn" onClick={()=> addNewTodo(taskValue, nameValue)}>Add Todo</button>
+                <button className="add-btn" onClick={()=> handleClick()}>Add Todo</button>
             </div>
         
     </div>
