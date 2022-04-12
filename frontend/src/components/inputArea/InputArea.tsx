2573def92f4b1d3a2ab756d09a04df51e4fb6bf5
CarinWood <carin.wood.85@gmail.com>
2022-04-12 14:47:10 +0200

    Update InputArea.tsx

--- frontend/src/components/inputArea/InputArea.tsx
+++ frontend/src/components/inputArea/InputArea.tsx
@@ -6,6 +6,11 @@ const inputArea:FC = () => {
   return (
     <div className="input-container">
         <h1 className='todo-heading'>Todo List <BsPencilFill/></h1>
+        <div className="input-fields">
+            <input className="input-task" placeholder='Task' />
+            <input className="input-name" placeholder='Your name'/>
+            <button className="add-btn">Add Todo</button>
+        </div>
 
   
           
