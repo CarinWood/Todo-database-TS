8730d28105fd1bd73f9b6f42c095486ba4cebd8b
CarinWood <carin.wood.85@gmail.com>
2022-04-12 18:51:47 +0200

    menu

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,6 +1,7 @@
 import './card.css'
 import { IoClose } from 'react-icons/io5';
 import TodoApiService from '../../api/services/TodoApiService'
+import {useState} from 'react'
 
 interface Props {
       task: string
@@ -8,15 +9,18 @@ interface Props {
       done: string
       id: string
       deleteTodo: (id: string) => void
-    
-
     }
 
 
 
 const Card:React.FC<Props> = ({task, name, done, id, deleteTodo}) => {
 
+const [showMenu, setShowMenu] = useState(false)
 
+const menuClick = () => {
+  setShowMenu(!showMenu)
+  console.log(showMenu)
+}
   
   
 
@@ -24,10 +28,20 @@ const Card:React.FC<Props> = ({task, name, done, id, deleteTodo}) => {
   return (
     <div className='card'>
         <div className='card-header'>
+              <div className='dot-area' onClick= {() => menuClick()}>
+                    ...
+              </div>
+
               <div className='close-area'>
                     <IoClose onClick={() => deleteTodo(id)} className='close'/>
               </div>
+            
         </div>
+        {showMenu === true && <section className='menu'>
+          <p>Edit Task</p>
+          <p>Mark as Completed</p>
+          <p>Mark as Uncompleted</p>
+        </section>}
         
         <h2 className='task-text'>{task}</h2>
         <h3 className='name-text'>{name}</h3>
